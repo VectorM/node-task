ca21b376b8d4b06548528bbe8e878bf6f9fbd489
VectorM <ua.itzakon@gmail.com>
2017-07-18 17:24:31 +0300

    config for mongoose

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from'express';
 import mongoose from 'mongoose';
+import session from 'express-session';
 import bodyParser from 'body-parser'
 import { base, technologies, practices } from './routes';
 
+import config from './config';
 const port = 3000;
 const app = express();
 
 mongoose.connect('mongodb://localhost/trainee')
 
 app.use(bodyParser());
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', base )
 app.use('/practices', practices)
---
+++ config/index.js
@@ -0,0 +1,5 @@
+const config = {
+  database: 'localhost/trainee',
+  port: 3000,
+  secret: 'verysecretkey'
+}
